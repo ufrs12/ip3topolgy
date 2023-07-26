59de510901f58b875db9391ebeda4c4eec076281
Alexey <ufrs12@yandex.ru>
2023-07-26 21:10:11 +0300

    1

--- ip3topogy/src/app/bankelements/services/cbank.service.ts
+++ ip3topogy/src/app/bankelements/services/cbank.service.ts
@@ -8,50 +8,128 @@ import { Elem } from '../models/element';
 export class CbankService {
   cbank:Elem[] = [];
   constructor() { 
-    this.cbank[0] = new Elem;
-    this.cbank[0] = {
+
+    this.cbank.push({
+      name: 'wire',
+      nnames: [],
+      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="M 67,100 V 0.13264933" id="path8542" sodipodi:nodetypes="cc" />'
+    })
+
+    this.cbank.push({
+      name: 'wireN',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:0.992701" d="M 80.872437,0.59174242 80.855377,99.834371" id="path2252" />'
+    })
+
+    this.cbank.push({
+      name: 'wireNdown',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:0.992701" d="M 67,79.999998 80.855377,99.834373" id="path2160" sodipodi:nodetypes="cc" />'
+    })
+
+    this.cbank.push({
+      name: 'wireNup',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:0.992701" d="M 67.017131,20.426117 80.872438,0.59174243" id="path2160-2" sodipodi:nodetypes="cc" />'
+    })
+
+    this.cbank.push({
+      name: 'wirePE',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:4.5;stroke-opacity:0.992701;stroke-dashoffset:0" d="M 89.470832,99.470831 89.48785,0.95528243" id="path2254" />'
+    })
+
+    this.cbank.push({
+      name: 'wirePEdown',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:4.5;stroke-dashoffset:5.0999999;stroke-opacity:0.992701" d="M 89.470833,99.470833 67,80" id="path1956" sodipodi:nodetypes="cc" />'
+    })
+
+    this.cbank.push({
+      name: 'wirePEup',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:4.5;stroke-dashoffset:5.0999999;stroke-opacity:0.992701" d="M 89.48785,0.95528243 67.017131,20.426115" id="path1956-5" sodipodi:nodetypes="cc" />'
+    })
+
+    this.cbank.push({
+      name: 'motor',
+      nnames: ['Motor 1L', 'Motor 3L'],
+      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="M 67,1.2130651 67,21.28209" id="path8544" sodipodi:nodetypes="cc" /><circle style="fill:none;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:5.1" id="path6094" cx="68.317619" cy="43.175556" r="21.893467" /><path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:19.2756;stroke-opacity:1" d="m 59.79032,50 0.17742,-15.225808 9.45838,8.401364 8.638394,-8.401365 L 78.241933,50" id="path7051" sodipodi:nodetypes="ccccc" />',
+      tx1: 10,
+      ty1: 20,
+    })
+
+    this.cbank.push({
       name: 'auto',
       nnames: ['Auto 1P', 'Auto 2P', 'Auto 3P'],
       svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="M 67,100.466 V 49.874316" id="path8542" sodipodi:nodetypes="cc" /><path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="M 67,0.68389843 V 21.282" id="path8544" sodipodi:nodetypes="cc" /><path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="M 67,49.874315 54.290322,23.064516" id="path8600" sodipodi:nodetypes="cc" /> <path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="m 57.974814,30.836585 -5.081999,2.527991 6.10751,12.706334 4.937526,-2.655893" id="path15417" sodipodi:nodetypes="cccc" />',
       tx1: 10,
       ty1: 20,
       text1: 'QF888',
-    }
+    })
 
-    this.cbank[1] = new Elem;
-    this.cbank[1] = {
-      name: 'wire',
-      nnames: [],
-      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="M 67,100 V 0.13264933" id="path8542" sodipodi:nodetypes="cc" />'
-    }
-
-    this.cbank[2] = new Elem;
-    this.cbank[2] = {
+    this.cbank.push({
       name: 'consumer',
       nnames: ['Consumer'],
       svg: '<rect width="100" height="100" fill="#ffcc33"></rect>'
-    }
-
-    this.cbank[3] = new Elem;
-    this.cbank[3] = {
+    })
+//Шина
+    this.cbank.push({
       name: 'busL',
       nnames: [],
       svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.9563;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:19.2756;stroke-opacity:0.969231" d="M 1.4566208,41.0937 H 100.543379" id="path8591" />'
-    }
-    this.cbank[4] = new Elem;
-    this.cbank[4] = {
+    })
+
+    this.cbank.push({
       name: 'busN',
       nnames: [],
-      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.9563;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:19.2756;stroke-opacity:0.969231" d="M 1.4566208,41.0937 H 100.543379" id="path8591" />'
-    }
+      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:1.8;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:19.2756;stroke-opacity:0.969231" d="M 1.6336237,48.638357 H 101" id="path8591-9" />'
+    })
 
-    this.cbank[5] = new Elem;
-    this.cbank[5] = {
-      name: 'motor',
-      nnames: ['Motor 1L', 'Motor 3L'],
-      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="M 68.1085,1.2130651 68.31762,21.28209" id="path8544" sodipodi:nodetypes="cc" /><circle style="fill:none;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:5.1" id="path6094" cx="68.317619" cy="43.175556" r="21.893467" /><path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-dashoffset:19.2756;stroke-opacity:1" d="m 59.79032,50 0.17742,-15.225808 9.45838,8.401364 8.638394,-8.401365 L 78.241933,50" id="path7051" sodipodi:nodetypes="ccccc" />',
-      tx1: 10,
-      ty1: 20,
-    }
+    this.cbank.push({
+      name: 'busPE',
+      nnames: [],
+      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:1.8;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:7.19999981;stroke-dashoffset:0;stroke-opacity:0.969231" d="M 1.4566203,55.604864 H 98.543379" id="path8591-9-3" />'
+    })
+    this.cbank.push({
+      name: 'busPE',
+      nnames: [],
+      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:1.8;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:7.19999981;stroke-dashoffset:0;stroke-opacity:0.969231" d="M 1.4566203,55.604864 H 98.543379" id="path8591-9-3" />'
+    })
+    this.cbank.push({
+      name: 'busLd',
+      nnames: [],
+      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="M 16.999999,41.199338 V 100.14515" id="path8542" sodipodi:nodetypes="cc" /><circle style="fill:#1f2607;fill-opacity:0.968627;stroke:none;stroke-width:1.01536;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:3.04608;stroke-dashoffset:3.45222;stroke-opacity:0.992701" id="path5891" cx="17" cy="41.0937" r="3" />'
+    })
+
+    this.cbank.push({
+      name: 'busLu',
+      nnames: [],
+      svg: '<path style="fill:none;fill-opacity:1;stroke:#1f2607;stroke-width:2.217;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1" d="M 66.999999,41.199338 V 0.66181593" id="path8542" sodipodi:nodetypes="cc" /><circle style="fill:#1f2607;fill-opacity:0.968627;stroke:none;stroke-width:1.01536;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:3.04608;stroke-dashoffset:3.45222;stroke-opacity:0.992701" id="path5891" cx="67" cy="41.0937" r="3" />'
+    })
+
+    this.cbank.push({
+      name: 'busNd',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:0.992701" d="m 30.867549,99.704167 -0.0171,-51.040704" id="path2252" sodipodi:nodetypes="cc" /><circle style="fill:#1f2607;fill-opacity:0.968627;stroke:none;stroke-width:1.01536;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:3.04608;stroke-dashoffset:3.45222;stroke-opacity:0.992701" id="path5891-9" cx="30.858999" cy="48.638283" r="3" />'
+    })
+
+    this.cbank.push({
+      name: 'busNu',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:0.992701" d="M 80.8671,0.75 80.85,48.663463" id="path2252" sodipodi:nodetypes="cc" /><circle style="fill:#1f2607;fill-opacity:0.968627;stroke:none;stroke-width:1.01536;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:3.04608;stroke-dashoffset:3.45222;stroke-opacity:0.992701" id="path5891-9" cx="80.858551" cy="48.638283" r="3" />'
+    })
+
+    this.cbank.push({
+      name: 'busPEd',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:4.5;stroke-dashoffset:0;stroke-opacity:0.992701" d="m 39.450489,59.653046 0.01702,44.359454" id="path2254" sodipodi:nodetypes="cc" /><circle style="fill:#1f2607;fill-opacity:0.968627;stroke:none;stroke-width:1.01536;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:3.04608;stroke-dashoffset:3.45222;stroke-opacity:0.992701" id="path5891-9-7" cx="39.459" cy="55.604862" r="3" />'
+    })
+
+    this.cbank.push({
+      name: 'busPEu',
+      nnames: [],
+      svg: '<path style="fill:none;stroke:#1f2607;stroke-width:1.5;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:4.5;stroke-dashoffset:0;stroke-opacity:0.992701" d="M 89.45,55.653046 89.46702,0" id="path2254" sodipodi:nodetypes="cc" /><circle style="fill:#1f2607;fill-opacity:0.968627;stroke:none;stroke-width:1.01536;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:3.04608;stroke-dashoffset:3.45222;stroke-opacity:0.992701" id="path5891-9-7" cx="89.458511" cy="55.604862" r="3" />'
+    })
   }
 }
--- ip3topogy/src/app/components/shema/services/scheme.service.ts
+++ ip3topogy/src/app/components/shema/services/scheme.service.ts
@@ -5,12 +5,14 @@ import { Nod } from 'src/app/models/node';
 import { GlobalService } from 'src/app/services/global.service';
 
 export interface Cell{
-  xoffset: number;    //смещение
+  xoffset?:number;   //смещение
   item?:   string;    //название элемента
   nod?:    Nod;       //год
   fsvg?:   string;    //полный SVG элемента
-  wireb?:  string[];  //начало провода
-  wiree?:  string[];  //конец провода
+  wireb?:  string[];  //начало основного провода
+  wiree?:  string[];  //конец основного провода
+  wn?:     boolean;   //нулевой провод
+  wp?:     boolean;   //защитный провод
 }
 @Injectable({
   providedIn: 'root'
@@ -18,6 +20,7 @@ export interface Cell{
 export class SchemeService {
   grid:Cell[][] = [[],[]];
   xoff:number = 0;
+  tree:Cell;
 
   constructor(
     public cbankService: CbankService,
@@ -26,18 +29,94 @@ export class SchemeService {
   Init() {
     if(this.globalService.tree.root){ //выходим, если нет рута
       this.grid.length = 0;
-      this.Construct(this.globalService.tree.root, 0, 0);
-      console.log(this.grid);
+
+      if (!this.grid[0]){this.grid.push([])};
+      this.grid[0][0] = {
+        item:'tree', 
+        wiree:this.CheckCont(this.globalService.tree.properties),
+      };
+      this.Construct(this.globalService.tree.root, 1, 0);
       this.DrawNodes();
       this.DrawBus();
       this.DrawWires();
+      console.log(this.grid);
     }
   }
-  DrawWires(){
-    for (var { row, r } of this.grid.map((row, r) => ({ row, r }))) {
-      for (var { cell, c } of row.map((cell, c) => ({ cell, c }))) {  
-        if (cell.nod){
 
+  DrawWires(){
+    for (var r = this.grid.length - 1; r >= 0; r--) {
+      for (var c = this.grid[r].length - 1; c >= 0; c--) {
+        let cell:Cell = this.grid[r][c];
+        if (cell.item == 'wire'){
+          cell.wiree = this.grid[r+1][c].wireb;
+          cell.wireb = this.grid[r-1][c].wiree;
+          if (this.grid[r-1][c].wiree.length > 0 && cell.wiree.length > 0 ){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='wire').svg;
+          }
+
+          let difference = cell.wiree.filter(x => !cell.wireb.includes(x));
+          if (difference.includes('N')){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='wireNup').svg;
+            this.grid[r-1][c].wn = true;
+            cell.wn = true; //для отрисовки шины
+          }
+
+          if (difference.includes('PE')){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='wirePEup').svg;
+            this.grid[r-1][c].wp = true;
+            cell.wp = true; //для отрисовки шины
+          }
+
+          difference = cell.wireb.filter(x => !cell.wiree.includes(x));
+          if (difference.includes('N')){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='wireNdown').svg;
+          }else{
+            if (this.grid[r+1][c].wn){
+              cell.fsvg += this.cbankService.cbank.find(e => e.name =='wireN').svg;
+              this.grid[r-1][c].wn = true;
+            }
+          }
+
+          if (difference.includes('PE')){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='wirePEdown').svg;
+          }else{
+            if (this.grid[r+1][c].wp){
+              cell.fsvg += this.cbankService.cbank.find(e => e.name =='wirePE').svg;
+              this.grid[r-1][c].wp = true;
+            }
+          }
+        }
+
+        if(cell.nod){
+          if(cell.wn){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='wireN').svg;
+            this.grid[r-1][c].wn = true;
+          }
+          if(cell.wp){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='wirePE').svg;
+            this.grid[r-1][c].wp = true;
+          }
+        }
+        if (cell.item == 'bus'){
+          cell.fsvg += this.cbankService.cbank.find(e => e.name =='busLd').svg;
+          if (c==0){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='busLu').svg;
+            if(cell.wn){
+              cell.fsvg += this.cbankService.cbank.find(e => e.name =='busNu').svg;
+            }
+            if(cell.wp){
+              cell.fsvg += this.cbankService.cbank.find(e => e.name =='busPEu').svg;
+            }
+            
+          }
+          
+          if(this.grid[r+1][c].wn){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='busNd').svg;
+          }
+          console.log (this.grid[r+1][c].wp);
+          if(cell.wp){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='busPEd').svg;
+          }
         }
       }
     }
@@ -49,7 +128,6 @@ export class SchemeService {
     let arr:string[] = [];
     for (var s in p) {
       if (ch.includes(s) && co.includes(p[s])){
-        //let asd:string = p[s];
         arr.push(p[s]);
       }
     } 
@@ -65,11 +143,20 @@ export class SchemeService {
         if (cell.item == 'bus'){
           if (phtext.join().length > 1){
             cell.fsvg += this.cbankService.cbank.find(e => e.name =='busL').svg;
+            cell.wireb = phtext;
+            cell.wiree = phtext;
+          }
+          if (this.CheckCont(this.globalService.tree.properties).includes('N')){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='busN').svg;
+            cell.wn = true;
+          }
+          if (this.CheckCont(this.globalService.tree.properties).includes('PE')){
+            cell.fsvg += this.cbankService.cbank.find(e => e.name =='busPE').svg;
+            cell.wp = true;
           }
         }
       }
     }
-    
   }
 
   DrawNodes(){
@@ -80,6 +167,8 @@ export class SchemeService {
           cell.fsvg += ce.svg;
           ce.text1 = cell.nod.title;
           cell.fsvg += Elem.getText1(ce);
+          cell.wireb = this.CheckCont(cell.nod.properties);
+          cell.wiree = this.CheckCont(cell.nod.properties)
         }
       }
     }
