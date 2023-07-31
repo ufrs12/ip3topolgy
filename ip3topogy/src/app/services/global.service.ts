7623aaa80d749c72c9dbdcfc3cd6f2cb2ddf6fff
Gantres1 <aporosin35@gmail.com>
2023-07-31 20:24:22 +0300

    1

--- ip3topogy/src/app/services/global.service.ts
+++ ip3topogy/src/app/services/global.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Nod } from '../models/node';
 import { Tree } from '../models/tree';
+import { Project } from '../models/project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
+
+  public project: Project = new Project;
+
+
   public strtree: string = '';
   public tree:    Tree;
   public nodes:   Nod[] = [];
